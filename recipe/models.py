8a8ec08b3524b5f3c6c1f821204284ae5ee8b6e3
Grace <unliszt@gmail.com>
2019-06-26 12:10:56 -0400

    My Django Girls app, first commit

---
+++ recipe/models.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    ##ingredients = models.ManyToManyField(Ingredients) ##NEED TO LOOK AT THIS AGAIN
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.title
