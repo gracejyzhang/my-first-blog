cb42ad0e54ad55a8c877b09224f45ca50242e536
Grace <unliszt@gmail.com>
2019-06-26 18:28:23 -0400

    Added view and template for detailed blog post as well as CSS for site."

--- recipe/views.py
+++ recipe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Recipe
 
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Recipe
 def recipe_list(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipe/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipe/recipe_detail.html', {'recipe': recipe})
